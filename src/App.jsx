0aed6fe4be4c2cac15a916f0bc31137fd9de05cd
diegonacif <diegoreisnacif@gmail.com>
2023-02-20 23:41:21 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header/Header';
+import './css/App.css';
+
+function App() {
+
+  return (
+    <div className="app-container">
+      <Header />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,8 @@
+export const Header = () => {
+  return (
+    <div className="header-container">
+      <h2>The Geek Store</h2>
+      <span>Carrinho de compras</span>
+    </div>
+  )
+}
