416fe2f728907860893cd8dc6acfd3423be37b0b
diegonacif <diegoreisnacif@gmail.com>
2023-02-22 11:41:59 -0300

    feat: promotion banner

--- src/components/Banners/Banners.jsx
+++ src/components/Banners/Banners.jsx
@@ -9,11 +9,13 @@ export const Banners = () => {
         modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
         spaceBetween={25}
         slidesPerView={1}
+        speed={600}
         navigation={{ clickable: true }}
         pagination={{ clickable: true, type: 'bullets' }}
         autoplay={{ delay: 5000 }}
         onSlideChange={() => console.log('slide change')}
         onSwiper={(swiper) => console.log(swiper)}
+        id="main-banner"
       >
         <SwiperSlide>
           <div className="carousel-item" id="carousel-item-1"></div>
@@ -26,6 +28,40 @@ export const Banners = () => {
         </SwiperSlide>
         <SwiperSlide>
           <div className="carousel-item" id="carousel-item-4"></div>
+        </SwiperSlide>
+      </Swiper>
+
+      <Swiper
+        modules={[Navigation, Pagination, Scrollbar, A11y, Autoplay]}
+        spaceBetween={25}
+        slidesPerView={1}
+        speed={600}
+        navigation={{ clickable: true }}
+        // pagination={{ clickable: true, type: 'bullets' }}
+        autoplay={{ delay: 5000 }}
+        onSlideChange={() => console.log('slide change')}
+        onSwiper={(swiper) => console.log(swiper)}
+        id="promotion-banner"
+      >
+        <SwiperSlide>
+          <div className="carousel-item">
+            <span>PARCELE 3X SEM JUROS</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="carousel-item">
+            <span>5% DESCONTO À VISTA</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="carousel-item">
+            <span>LOJA 100% SEGURA</span>
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="carousel-item">
+            <span>ENTREGA EM TODO BRASIL</span>
+          </div>
         </SwiperSlide>
       </Swiper>
     </div>
