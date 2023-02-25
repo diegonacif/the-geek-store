1c35fa0fa0b1ba0a9fba3aadd16e9ed8b590c698
diegonacif <diegoreisnacif@gmail.com>
2023-02-24 23:29:17 -0300

    feat: cart items price sum

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,12 +2,15 @@ import { ArrowLeft } from "phosphor-react";
 import { Cartitem } from "../CartItem/Cartitem";
 
 import "../../css/App.css";
+import { Link } from "react-router-dom";
 
 export const Cart = () => {
   return (
     <div className="cart-container">
       <header>
-        <ArrowLeft size={28} color="#154854" weight="duotone" />
+        <Link to="/">
+          <ArrowLeft size={28} color="#154854" weight="duotone" />
+        </Link>
         <div className="cart-header-wrapper">
           <h3>Carrinho</h3>
           <span>(1)</span>
--- src/components/CartItem/Cartitem.jsx
+++ src/components/CartItem/Cartitem.jsx
@@ -1,17 +1,41 @@
+import { useEffect, useState } from 'react';
 import Img from '../../assets/camiseta1.webp';
 
 import "../../css/App.css";
 
 export const Cartitem = () => {
+
+  const [quantity, setQuantity] = useState(1);
+  const [calculatedPrice, setCalculatedPrice] = useState(0);
+  const temporaryPrice = 52.90;
+
+  useEffect(() => {
+    setCalculatedPrice(quantity * temporaryPrice)
+  }, [quantity])
+
+  const handleChangeQuantity = (type) => {
+    if(type === 'plus') {
+      return setQuantity(current => current + 1);
+    } else if(type === 'minus') {
+      return setQuantity(current => current - 1);
+    } else {
+      return;
+    }
+  }
+  
   return (
     <div className="cart-item-container">
       <img src={Img} alt="" />
       <div className="cart-item-info">
         <h4>T-shirt Ursinhos Carinhosos</h4>
-        <span>R$ 999,90</span>
+        <span>Tamanho único</span>
         <div className="cart-item-sum">
-          <input type="number" />
-          <span>Sub-total: R$ 9999,90</span>
+          <div className="cart-input-wrapper">
+            <button onClick={() => handleChangeQuantity('minus')}>-</button>
+            <input type="number" value={quantity} readOnly />
+            <button onClick={() => handleChangeQuantity('plus')}>+</button>
+          </div>
+          <span>R$ {calculatedPrice}</span>
         </div>
       </div>
 
