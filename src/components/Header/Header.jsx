83266453dbe188e8cb3ff66804a0d27665675a83
diegonacif <diegoreisnacif@gmail.com>
2023-02-26 15:20:11 -0300

    feat: header logout dinamic button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,31 @@
 import { Link } from 'react-router-dom';
-import { List, ShoppingCart, User } from "phosphor-react"
+import { List, ShoppingCart, SignOut, User } from "phosphor-react"
+import { useContext } from 'react';
+import { AuthEmailContext } from '../../contexts/AuthEmailProvider';
 import '../../css/App.css';
 
 export const Header = () => {
+  // Email Context
+  const { 
+    isSignedIn,
+    logoutUser
+  } = useContext(AuthEmailContext);
   return (
     <div className="header-container">
       
       <List size={28} color="#f0f0f0" weight="duotone" />
       <span>The Geek Store</span>
       <div className="header-utilities">
-        <Link to="/login">
-          <User size={28} color="#f0f0f0" weight="duotone" />
-        </Link>
         <Link to="/cart">
           <ShoppingCart size={28} color="#f0f0f0" weight="duotone" />
         </Link>
+        {
+          isSignedIn ?
+          <SignOut onClick={() => logoutUser()} size={28} color="#f0f0f0" weight="duotone" /> :
+          <Link to="/login">
+            <User size={28} color="#f0f0f0" weight="duotone" />
+          </Link>
+        }
       </div>
     </div>
   )
